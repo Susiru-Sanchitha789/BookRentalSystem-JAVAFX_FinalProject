8e61e8a72503561904d583a3f211549453faca87
Susiru-Sanchitha789 <sanchithasusiru205@gmail.com>
2025-12-10 23:31:11 +0530

    commite

--- src/main/java/Controller/LoginController/LoginPageController.java
+++ src/main/java/Controller/LoginController/LoginPageController.java
@@ -1,10 +1,11 @@
-package Controller;
+package Controller.LoginController;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
-public class LoginPageController {
+
+public class LoginPageController  {
 
     @FXML
     private TextField AdminStaffid;
---
+++ src/main/java/model/dto/LoginMember.java
@@ -0,0 +1,18 @@
+package model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class LoginMember {
+    private String name;
+
+    private String password;
+
+    private String position;
+}

e10951bc452d2e0d3eb6b91b65debc1307ac42fb
Susiru-Sanchitha789 <sanchithasusiru205@gmail.com>
2025-12-10 23:03:50 +0530

    commite

--- src/main/java/Controller/LoginPageController.java
+++ src/main/java/Controller/LoginPageController.java
@@ -1,4 +1,23 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class LoginPageController {
+
+    @FXML
+    private TextField AdminStaffid;
+
+    @FXML
+    private TextField Passwordid;
+
+    @FXML
+    private TextField Usernameid;
+
+    @FXML
+    void LoginOnAction(ActionEvent event) {
+
+    }
+
 }
